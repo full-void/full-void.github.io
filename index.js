e81ef42602f97bbbd0f0876da2271eb13567c283
Abhinav Garg <maintoabhinavgarg@gmail.com>
2021-10-10 22:42:17 -0400

    Added more info, changed the design

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function optimal_em() {
     console.log("Window width", $(window).width());
     let fsz = 1.5 * $(window).width() / 100;
     console.log(fsz);
-    if (fsz < 17){
+    if (fsz < 17) {
         fsz = 17;
     }
     if (fsz > 21) {
@@ -12,13 +12,13 @@ function optimal_em() {
     console.log("The value", $("body").css("font-size"))
 }
 
-function scrollFadeIn (){
-    $('.zero-opacity').each( function(i){
+function scrollFadeIn() {
+    $('.zero-opacity').each(function (i) {
         let top_of_object = $(this).offset().top;
         let top_of_window = $(window).scrollTop();
         let bottom_of_object = top_of_object + $(this).outerHeight();
         let bottom_of_window = top_of_window + $(window).height();
-        if( bottom_of_window > top_of_object && top_of_window < bottom_of_object ){
+        if (bottom_of_window > top_of_object && top_of_window < bottom_of_object) {
             $(this).addClass('full-opacity');
         }
         else {
@@ -27,11 +27,43 @@ function scrollFadeIn (){
     });
 }
 
-$(document).ready(function() {
+function scrollFunc() {
+    scrollFadeIn();
+    navColoring();
+}
+
+$(document).ready(function () {
     optimal_em();
     scrollFadeIn();
-    $(window).scroll(scrollFadeIn);
+    $(window).scroll(scrollFunc);
 
     $(window).on('resize', optimal_em);
     $(window).on('resize', scrollFadeIn);
 });
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".nav .nav-item");
+
+function navColoring() {
+    console.log('debug.sections', sections);
+    console.log('debug.NavLi', navLi);
+
+    var current = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (pageYOffset >= sectionTop - 60) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    console.log('debug.current', current);
+
+
+    navLi.forEach((li) => {
+        li.classList.add("text-black-nav");
+        if (li.classList.contains(current)) {
+            li.classList.remove("text-black-nav");
+        }
+    });
+};
